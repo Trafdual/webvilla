39420d61b174e58c12fe60b4333d7c4c0eb54262
trafdual <traz08102003@gmail.com>
2025-05-07 14:16:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+const passport = require('passport')
+var path = require('path')
+
+const port = process.env.PORT || 8080
+
+
+var app = express()
+app.use(methodOverride('_method'))
+
+const uri =
+  'mongodb+srv://webnhansu123:webnhansu123@cluster0.rxpru.mongodb.net/webvilla?retryWrites=true&w=majority'
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log('kết nối thành công'))
+
+const mongoStoreOptions = {
+  mongooseConnection: mongoose.connection,
+  mongoUrl: uri,
+  collection: 'sessions'
+}
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/uploads')))
+app.use(express.static(path.join(__dirname, '/images')))
+
+app.use(
+  session({
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create(mongoStoreOptions),
+    cookie: {
+      secure: false
+    }
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(cors())
+
+app.listen(port, () => {
+  try {
+    console.log('kết nối thành công 8080')
+  } catch (error) {
+    console.log('kết nối thất bại 8080', error)
+  }
+})
+
